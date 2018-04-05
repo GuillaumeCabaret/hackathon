e47e7e7eb2cbc6aaf326e70c83ab2f21e5a31c5b
Benbachir Azzédine <azzedine.benbachir@gmail.com>
2018-04-05 16:12:43 +0200

    start routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
 
-
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GameComponent, HomeComponent]
 })
 export class AppModule { }
